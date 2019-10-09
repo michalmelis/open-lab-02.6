b7b84f67daca85c4791f8951768690b5e4698de8
michalmelis <michalmelis.sk@gmail.com>
2019-10-09 20:02:22 +0200

    open-lab-02.6

---
+++ open-lab-02.6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace open_lab_02._6
+{
+    class Program
+    {
+        public static bool Divisible(int number)
+        {
+            if (number % 100 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
